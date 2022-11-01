02fc866b08a354c2732cd595980c4759a52b7a63
cburnside1385 <chrisburnside13@gmail.com>
2022-11-01 19:47:37 -0400

    Updated to horizontal scrolling

--- Develop/client/src/pages/Popular.js
+++ Develop/client/src/pages/Popular.js
@@ -6,7 +6,9 @@ import { start } from '../utils/API';
 
 
 const Popular = () => {
-    const [populars,popularmovieapi] = useState([]);
+    const [populars, popularmovieapi] = useState([]);
+    const [rating, setRating] = useState(0);
+    const [hover, setHover] = useState(0);
     const Popularfilms = async () => {
       
 
@@ -43,18 +45,18 @@ const Popular = () => {
       
 
             
-            <>
+        <div className="container-fluid" >
 
 
-                <Container className="sideScroll">
+            
 
-                    <div class="form-row text-center">
+                <div className="text-center scrolling-wrapper row flex-row flex-nowrap mt-4 pb-4 pt-2">
 
                     {populars.map((movie) => {
 
                             return (
-                                <div className="card">
-
+                                <><div className="card col-2">
+                                    <div className={ (hover || rating) ? "saved heart" : "notsaved heart"}>&#x2665;</div>
                                     <img className="img1" src={`https://image.tmdb.org/t/p/original/${movie.poster}`} />
                                     <center>
                                         <div className="img2">{movie.title}</div>
@@ -62,14 +64,32 @@ const Popular = () => {
                                     <div className="text">{movie.overview}</div>
 
                                 </div>
+                                        <div className="star-rating">
+                                            {[...Array(5)].map((star, index) => {
+                                                index += 1;
+                                                return (
+                                                    <button
+                                                        type="button"
+                                                        key={index}
+                                                        className={index <= (hover || rating) ? "on" : "off"}
+                                                        onClick={() => setRating(index)}
+                                                        onMouseEnter={() => setHover(index)}
+                                                        onMouseLeave={() => setHover(rating)}
+                                                    >
+                                                        <span className="star">&#9733;</span>
+                                                    </button>
+                                                );
+                                            })}
+                                        </div>
+                                    </>
 
                             );
                         })}
 
 
                     </div>
-                </Container>
-            </>
+                
+            </div>
         
 
     );
