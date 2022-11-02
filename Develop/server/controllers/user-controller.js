f81d2d89ba3a2b8223fe1474b161dee83761d15d
Andrew Gettz <drews_computer@Andrews-MacBook-Pro.local>
2022-11-01 20:05:56 -0400

    updates to the controller, return correct pw

--- Develop/server/controllers/user-controller.js
+++ Develop/server/controllers/user-controller.js
@@ -26,5 +26,19 @@ async createUser({body}, res) {
     }
     const token = signToken(user); 
     res.json({token, user}); 
-},    
+}, 
+async login({ body }, res) {
+    const user = await User.findOne({ $or: [{ username: body.username }, { email: body.email }] });
+    if (!user) {
+      return res.status(400).json({ message: "Can't find this user" });
+    }
+
+    const correctPw = await user.isCorrectPassword(body.password);
+
+    if (!correctPw) {
+      return res.status(400).json({ message: 'Wrong password!' });
+    }
+    const token = signToken(user);
+    res.json({ token, user });
+  },   
 }
